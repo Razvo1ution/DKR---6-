f9bc99e731f8fcb2ce872029848b1a6bcdfe4f14
Razvo1ution <117993907+Razvo1ution@users.noreply.github.com>
2023-03-16 09:01:10 +0300

    Add files via upload

---
+++ дкр6.pas
@@ -0,0 +1,30 @@
+﻿type
+NodePtr = ^Node;
+Node = record
+data: Integer;
+next: NodePtr;
+end;
+
+var
+head: NodePtr;
+node1, node2, node3: Node;
+
+begin
+//Узлыыыыыыыы
+node1.data := 10;
+node2.data := 20;
+node3.data := 30;
+
+// созд Cвязный список
+head := @node1;
+node1.next := @node2;
+node2.next := @node3;
+node3.next := nil;
+
+// исп св список
+while head <> nil do
+begin
+writeln(head^.data);
+head := head^.next;
+end;
+end.
